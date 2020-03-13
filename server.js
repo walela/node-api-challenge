26a6e4126b9fefec3e18b93917ff021cfa501ad9
Austin Walela <austinmasibo@gmail.com>
2020-03-13 11:56:21 +0300

    added logging middleware

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const cors = require('cors')
 
 const server = express()
 
+const logger = (req, _, next) => {
+    const datetime = new Date().toGMTString()
+    console.log(`${req.method} ${req.url} ${datetime}`)
+    next()
+  }
+
 server.use(express.json())
 server.use(cors())
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send('<h1>Hello 😈!</h1>')
